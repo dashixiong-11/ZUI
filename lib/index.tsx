9a662b8259d1326bc7dddbbdf73d8d146d96ea3e
dashixiong <2788347834@qq.com>
2020-08-09 23:01:19 +0800

    发布zui

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,17 +1,1 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e);
-};
-
-const app = () => {
-    return <div>
-        <Icon  name='wechat'  color='red' onClick={fn}></Icon>
-    </div>;
-};
-
-
-ReactDOM.render(app(), document.getElementById('root'));
-
+export { default as Icon} from './icon/icon'
